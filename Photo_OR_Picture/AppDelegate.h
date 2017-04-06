42844e0fce80fff5f6273c4935d4ac340cbd8fc4
wu rui <wurui@secoo.com>
2017-04-06 18:03:45 +0800

    Initial Commit

---
+++ Photo_OR_Picture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photo_OR_Picture
+//
+//  Created by wurui on 17/4/6.
+//  Copyright © 2017年 wurui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
